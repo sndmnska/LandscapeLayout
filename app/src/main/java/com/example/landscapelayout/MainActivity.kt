e40a0a4a043ed8ce7e5f43c35d626c9ec66d7565
dan <email@email.com>
2023-02-04 00:52:27 -0600

    Initial commit

---
+++ app/src/main/java/com/example/landscapelayout/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.landscapelayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var seekBar: SeekBar
+    private lateinit var seekBarLabel: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        seekBar = findViewById(R.id.seek_bar)
+        seekBarLabel = findViewById(R.id.seek_bar_label)
+
+        val initialProgess = seekBar.progress
+        updateLabel(initialProgess)
+
+        // add listener to update label as seekbar changes.
+
+        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+            // notifies about start, live listen, and stop.
+            override fun onProgressChanged(seekBarComponent: SeekBar?, progress: Int, fromUser: Boolean) {
+                updateLabel(progress)
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+        })
+
+    }
+
+    private fun updateLabel(progress: Int) {
+        seekBarLabel.text = getString(R.string.seekbar_value_message, progress)
+    }
+}
