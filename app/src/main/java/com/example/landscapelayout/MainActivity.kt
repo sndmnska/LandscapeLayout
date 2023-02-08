2878762103ad50c8dd480cbc04f9b55b0fab9653
dan <email@email.com>
2023-02-08 11:28:11 -0600

    Tap the Square: Complete

--- app/src/main/java/com/example/landscapelayout/MainActivity.kt
+++ app/src/main/java/com/example/landscapelayout/MainActivity.kt
@@ -1,13 +1,32 @@
 package com.example.landscapelayout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.SeekBar
+import android.widget.Switch
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.material.snackbar.Snackbar
 
+// Data we need to send is the Seekbar's progress, using Extras (key:value pair(s))
+// Use global constants
+const val EXTRA_SQUARE_SIZE = "com.example.landscapelayout.tap_the_square.SQUARE_SIZE" // does not need to match file names..
+const val EXTRA_EASY_MODE = "com.example.landscapelayout.tap_the_square.EASY_MODE"
 class MainActivity : AppCompatActivity() {
     private lateinit var seekBar: SeekBar
     private lateinit var seekBarLabel: TextView
+    private lateinit var showSquareButton: Button
+    private lateinit var easyModeSwitch: Switch
+
+    private val squareResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            handleSquareResult(result)
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,15 +34,21 @@ class MainActivity : AppCompatActivity() {
 
         seekBar = findViewById(R.id.seek_bar)
         seekBarLabel = findViewById(R.id.seek_bar_label)
+        showSquareButton = findViewById(R.id.show_square_button)
+        easyModeSwitch = findViewById(R.id.easyModeSwitch)
 
-        val initialProgess = seekBar.progress
-        updateLabel(initialProgess)
+        val initialProgress = seekBar.progress
+        updateLabel(initialProgress)
 
         // add listener to update label as seekbar changes.
 
-        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             // notifies about start, live listen, and stop.
-            override fun onProgressChanged(seekBarComponent: SeekBar?, progress: Int, fromUser: Boolean) {
+            override fun onProgressChanged(
+                seekBarComponent: SeekBar?,
+                progress: Int,
+                fromUser: Boolean
+            ) {
                 updateLabel(progress)
             }
 
@@ -31,9 +56,49 @@ class MainActivity : AppCompatActivity() {
             override fun onStopTrackingTouch(p0: SeekBar?) {}
         })
 
+        showSquareButton.setOnClickListener {
+            showSquare()
+        }
+
+    }
+
+    private fun showSquare() {
+        /** launch the SquareActivity */
+        // telling Kotlin to find the SquareActivity class, an extension of the java class
+        // "this" refers to MainActivity object, references what to go back to!
+/*
+        Intent (this, SquareActivity::class. java).apply {
+            putExtra(EXTRA_SQUARE_SIZE, seekBar.progress)
+            squareResultLauncher.launch(this)
+*/
+        val easyMode = easyModeSwitch.isChecked;
+
+        // tell SquareActivity how large the square should be
+        // based on the progress (setting) of the Seekbar
+        val showSquareIntent = Intent(this, SquareActivity::class.java)
+        showSquareIntent.putExtra(EXTRA_EASY_MODE, easyMode)
+        showSquareIntent.putExtra(EXTRA_SQUARE_SIZE, seekBar.progress)
+//        startActivity(showSquareIntent)
+        squareResultLauncher.launch(showSquareIntent)
     }
 
     private fun updateLabel(progress: Int) {
         seekBarLabel.text = getString(R.string.seekbar_value_message, progress)
+    }
+
+    private fun handleSquareResult(result: ActivityResult) {
+        if (result.resultCode == RESULT_OK) {
+            val intent = result.data
+            val tapped = intent?.getBooleanExtra(EXTRA_TAPPED_INSIDE_SQUARE, false) ?: false
+            val message = if (tapped) {
+                getString(R.string.tapped_square)
+            } else {
+                getString(R.string.missed_square)
+            }
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        } else if (result.resultCode == RESULT_CANCELED) {
+            Toast.makeText(this, getString(R.string.did_not_try),
+                Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/example/landscapelayout/SquareActivity.kt
+++ app/src/main/java/com/example/landscapelayout/SquareActivity.kt
@@ -1,11 +1,51 @@
 package com.example.landscapelayout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
 
+const val EXTRA_TAPPED_INSIDE_SQUARE = "com.example.landscapelayout.tap_the_square.TAPPED_INSIDE_SQUARE"
 class SquareActivity : AppCompatActivity() {
+
+    private lateinit var squareImage: ImageView
+    private lateinit var container: View
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_square)
+
+        squareImage = findViewById(R.id.square)
+        container = findViewById(R.id.container)
+
+        // "intent" references the intent that brought us to this Activity in the first place.
+        var squareSize = intent.getIntExtra(EXTRA_SQUARE_SIZE, 100)
+        if (squareSize == 0) { squareSize = 1}
+
+        val easyMode = intent.getBooleanExtra(EXTRA_EASY_MODE, false)
+        if (easyMode) { squareSize *= 3 }
+
+        squareImage.layoutParams.width = squareSize
+        squareImage.layoutParams.height = squareSize
+
+        squareImage.setOnClickListener{
+            squareTapped(true)
+        }
+
+        container.setOnClickListener{
+            squareTapped(false)
+        }
+
+
+
+    }
+
+    private fun squareTapped(didTapSquare: Boolean) {
+        // end square activity
+        val resultIntent = Intent()
+        resultIntent.putExtra(EXTRA_TAPPED_INSIDE_SQUARE, didTapSquare)
+        setResult(RESULT_OK, resultIntent) // user attempted to tap square, did not "cancel"
+        finish() // end this activity
     }
 }
